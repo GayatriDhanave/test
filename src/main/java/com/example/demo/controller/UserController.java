1e2b7e6e26aa2c429c2d916d2ab798601ce43942
GayatriDhanave <gayatridhanave7@gmail.com>
2025-08-03 17:49:18 +0530

    changes

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -19,7 +19,7 @@ public class UserController {
     public String saveUser(@RequestBody Users users){
         boolean ans= userService.addUser(users);
         if(ans){return "User added"; }
-        System.out.println("This is main branch")
+        System.out.println("This is feature1 branch")
         return "User not added";
     }
 
