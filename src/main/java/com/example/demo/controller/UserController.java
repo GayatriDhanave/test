b031e870a889a8568ce72397677a346697d20ff7
GayatriDhanave <gayatridhanave7@gmail.com>
2025-08-03 23:01:20 +0530

    user controller addres

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Users;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.http.HttpResponse;
+
+
+@RestController
+public class UserController {
+
+
+    @Autowired
+    protected UserService userService;
+
+    @PostMapping("/addUser")
+    public String saveUser(@RequestBody Users users){
+        boolean ans= userService.addUser(users);
+        if(ans){return "User added"; }
+        return "User not added";
+    }
+
+    @DeleteMapping("/displayUser")
+    public String displayUser(@RequestBody Users users){
+        boolean ans= userService.addUser(users);
+        if(ans){return "User added"; }
+        return "User not added";
+    }
+}
